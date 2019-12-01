44a159daedb5ae55af81a2729c9403fc6ffd2d57
yoyo-simplon <47659202+yoyo-simplon@users.noreply.github.com>
2019-12-02 02:28:43 +0400

    projet mini chat

---
+++ server.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var mongoose = require('mongoose');
+
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
+
+
+
+var Message = mongoose.model('Message',{name : String, message : String});
+var dbUrl = 'mongodb://127.0.0.1:27017/simple-chat';
+
+
+var server = app.listen(3000, () => {
+    console.log('server is running on port', server.address().port);});
+
+ 
+
+    mongoose.connect(dbUrl, (err) => {
+        console.log('mongodb connected', err);
+    })
+
+    app.get('/messages', (req, res) => {
+        Message.find({},(err, messages)=> {
+            res.send(messages);
+        })
+    })
+
+    app.post('/messages', (req, res) => {
+        var message = new Message(req.body);
+        message.save((err) =>{
+            if(err)
+            sendStatus(500);
+            io.emit('message', req.body);
+            res.sendStatus(200);
+                })
+    })
+
+    io.on('connection', () =>{
+        console.log('a user is connected');
+    })
